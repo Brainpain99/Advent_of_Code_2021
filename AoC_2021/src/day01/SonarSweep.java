63a7423acba1c70303a244e9a2a75c21d974afa6
Daniel Tenbrink <Daniel Tenbrink@192.168.178.60>
2022-01-31 20:43:26 +0100

    Changes and improvements

--- AoC_2021/src/day01/SonarSweep.java
+++ AoC_2021/src/day01/SonarSweep.java
@@ -7,7 +7,34 @@ import java.util.Scanner;
 
 public class SonarSweep {
 
-	public static void main(String[] args) {
+	
+	public static void firststep() {
+		Scanner scan = null;
+		int result = 0;
+		ArrayList<Integer> input = new ArrayList<Integer>();
+		
+		try {
+			scan = new Scanner(new File("input.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while (scan.hasNext()) {
+			input.add(scan.nextInt());
+		}
+		
+		for(int i = 0; i < input.size()-1; i++) {
+
+			if(input.get(i+1) > input.get(i)) {
+				result++;
+			}
+		}
+		
+		scan.close();
+		System.out.println("Gesamt: "+result);
+	}
+	
+	public static void secondstep() {
 		Scanner scan = null;
 		int result = 0;
 		ArrayList<Integer> input = new ArrayList<Integer>();
@@ -33,6 +60,11 @@ public class SonarSweep {
 		
 		scan.close();
 		System.out.println("Gesamt: "+result);
+	}
+
+	public static void main(String[] args) {
+		firststep();
+		secondstep();
 	}
 
 }
